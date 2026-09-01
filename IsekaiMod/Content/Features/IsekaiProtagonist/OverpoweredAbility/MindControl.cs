493cc5364bcd0dfa25027120ecb363ea789e3d49
martin aponte <martin@astrosirens.com>
2026-09-01 00:17:56 -0500

    Fix Mind Control saves and daily resource
    Add a persistent Mind Control resource and attach it to both the ability and granting feature. Uses start at one, increase by one per five total character levels, restore with the feature, and cap at five.
    
    Make the initial effect allow a Will save to negate. On failure, the target joins the caster's faction for one round per character level; when control ends, the target gains one minute of protection from being controlled again. The custom implementation continues to bypass ordinary mind-affecting and compulsion immunity.

--- IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/MindControl.cs
+++ IsekaiMod/Content/Features/IsekaiProtagonist/OverpoweredAbility/MindControl.cs
@@ -34,7 +34,25 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
             LocalizedString MindControlDesc = Helpers.CreateString(IsekaiContext, "MindControl.Description",
                 "Behold the power of a king! All will listen and obey!"
                 + "\nBenefit: You can make any creature fight on your side as if it was your ally. "
-                + "It will {g|Encyclopedia:Attack}attack{/g} your opponents to the best of its ability.");
+                + "It will {g|Encyclopedia:Attack}attack{/g} your opponents to the best of its ability. "
+                + "A successful Will save negates this effect. This power bypasses immunity to mind-affecting and compulsion effects.");
+
+            var MindControlResource = Helpers.CreateBlueprint<BlueprintAbilityResource>(IsekaiContext, "MindControlResource", resource => {
+                resource.m_MaxAmount = new BlueprintAbilityResource.Amount() {
+                    BaseValue = 1,
+                    IncreasedByLevelStartPlusDivStep = true,
+                    StartingLevel = 0,
+                    StartingIncrease = 0,
+                    LevelStep = 5,
+                    PerStepIncrease = 1,
+                    MinClassLevelIncrease = 0,
+                    m_ClassDiv = new BlueprintCharacterClassReference[0],
+                    m_ArchetypesDiv = new BlueprintArchetypeReference[0],
+                    OtherClassesModifier = 1.0f
+                };
+                resource.m_UseMax = true;
+                resource.m_Max = 5;
+            });
 
             // Immunity buff
             var MindControlImmunity = TTCoreExtensions.CreateBuff("MindControlImmunity", bp => {
@@ -73,26 +91,23 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
                 bp.SetDescription(MindControlDesc);
                 bp.m_Icon = Icon_MindControl;
                 bp.AddComponent<AbilityEffectRunAction>(c => {
-                    c.Actions = ActionFlow.DoSingle<Conditional>(c => {
-                        c.ConditionsChecker = ActionFlow.IfSingle<ContextConditionHasFact>(c => {
-                            c.m_Fact = MindControlImmunity.ToReference<BlueprintUnitFactReference>();
-                        });
-                        c.IfTrue = ActionFlow.DoSingle<ContextActionApplyBuff>(c => {
-                            c.m_Buff = MindControlImmunity.ToReference<BlueprintBuffReference>();
-                            c.DurationValue = new ContextDurationValue() {
-                                Rate = DurationRate.Minutes,
-                                DiceType = DiceType.Zero,
-                                BonusValue = new ContextValue() { ValueType = ContextValueType.Simple, Value = 1 }
-                            };
-                        });
-                        c.IfFalse = ActionFlow.DoSingle<ContextActionApplyBuff>(c => {
-                            c.m_Buff = MindControlBuff.ToReference<BlueprintBuffReference>();
-                            c.DurationValue = new ContextDurationValue() {
-                                Rate = DurationRate.Rounds,
-                                DiceType = DiceType.Zero,
-                                DiceCountValue = 0,
-                                BonusValue = new ContextValue() { ValueType = ContextValueType.Rank }
-                            };
+                    c.SavingThrowType = SavingThrowType.Will;
+                    c.Actions = ActionFlow.DoSingle<ContextActionConditionalSaved>(c => {
+                        c.Succeed = ActionFlow.DoNothing();
+                        c.Failed = ActionFlow.DoSingle<Conditional>(c => {
+                            c.ConditionsChecker = ActionFlow.IfSingle<ContextConditionHasFact>(c => {
+                                c.m_Fact = MindControlImmunity.ToReference<BlueprintUnitFactReference>();
+                            });
+                            c.IfTrue = ActionFlow.DoNothing();
+                            c.IfFalse = ActionFlow.DoSingle<ContextActionApplyBuff>(c => {
+                                c.m_Buff = MindControlBuff.ToReference<BlueprintBuffReference>();
+                                c.DurationValue = new ContextDurationValue() {
+                                    Rate = DurationRate.Rounds,
+                                    DiceType = DiceType.Zero,
+                                    DiceCountValue = 0,
+                                    BonusValue = new ContextValue() { ValueType = ContextValueType.Rank }
+                                };
+                            });
                         });
                     });
                 });
@@ -104,13 +119,7 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
                     c.m_BaseValueType = ContextRankBaseValueType.CharacterLevel;
                 });
                 bp.AddComponent<AbilityResourceLogic>(c => {
-                    c.m_RequiredResource = Helpers.CreateBlueprint<BlueprintAbilityResource>(IsekaiContext, "MindControlResource", resource => {
-                        resource.m_MaxAmount = new BlueprintAbilityResource.Amount() {
-                            BaseValue = 1,
-                            IncreasedByLevel = true,
-                            LevelIncrease = 1 // Gains 1 additional use per level
-                        };
-                    }).ToReference<BlueprintAbilityResourceReference>();
+                    c.m_RequiredResource = MindControlResource.ToReference<BlueprintAbilityResourceReference>();
                     c.m_IsSpendResource = true;
                 });
                 bp.Type = AbilityType.Supernatural;
@@ -129,6 +138,10 @@ namespace IsekaiMod.Content.Features.IsekaiProtagonist.OverpoweredAbility {
                 bp.m_Icon = Icon_MindControl;
                 bp.AddComponent<AddFacts>(c => {
                     c.m_Facts = new BlueprintUnitFactReference[] { MindControlAbility.ToReference<BlueprintUnitFactReference>() };
+                });
+                bp.AddComponent<AddAbilityResources>(c => {
+                    c.m_Resource = MindControlResource.ToReference<BlueprintAbilityResourceReference>();
+                    c.RestoreAmount = true;
                 });
             });
 
